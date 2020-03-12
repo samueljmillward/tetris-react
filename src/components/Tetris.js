c6dfa07d48c80ae53b0477818c263c42d254a555
samueljmillward <samuel.j.pearce@hotmail.com>
2020-03-12 22:57:34 +0000

    reversed drop and rotate buttons

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -49,13 +49,13 @@ const Tetris = () => {
 
   const move = ({ keyCode }) => {
     if (!gameOver) {
-      if (keyCode === 37) {
+      if (keyCode === 38) {
         movePlayer(-1);
       } else if (keyCode === 39) {
         movePlayer(1);
       } else if (keyCode === 40) {
         dropPlayer();
-      } else if (keyCode === 38) {
+      } else if (keyCode === 37) {
         playerRotate(stage, 1);
       }
     }
